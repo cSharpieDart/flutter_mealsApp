db271c4a0d9342d3a63fe72aa2776c816ba97760
Ryan <xxiryanixx@hotmail.com>
2020-06-16 18:39:34 +1000

    added the meal item screen widget

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/meal_item.dart';
+import './data/data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -10,15 +12,24 @@ class CategoryMealsScreen extends StatelessWidget {
 
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: ListView.builder(itemBuilder: (context,index){
-
-      }
-      itemCount: ,
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return MealItem(
+              title: categoryMeals[index].title,
+              imageUrl: categoryMeals[index].imageUrl,
+              duration: categoryMeals[index].duration,
+              complexity: categoryMeals[index].complexity,
+              affordability: categoryMeals[index].affordability);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
