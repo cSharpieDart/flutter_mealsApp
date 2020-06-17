1b1dc5adc1241728db62ec6a1edfe9893d44a65e
Ryan <xxiryanixx@hotmail.com>
2020-06-17 18:27:53 +1000

    added ingredients to screen

--- lib/meal_detail_screen.dart
+++ lib/meal_detail_screen.dart
@@ -1,19 +1,68 @@
 import 'package:flutter/material.dart';
 
+import 'data/data.dart';
+
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
+
+  // Widget buildSectionTitle(String text){
+
+  // }
+
 
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
 
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("$mealId"),
-      ),
-      body: Center(
-        child: Text('The meal ${mealId}'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("${selectedMeal.title}"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                selectedMeal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(
+                vertical: 10,
+              ),
+              child: Text(
+                'Ingredients',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              height: 150,
+              width: 300,
+              child: ListView.builder(
+                itemBuilder: (context, index) => Card(
+                  color: Theme.of(context).accentColor,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      selectedMeal.ingredients[index],
+                    ),
+                  ),
+                ),
+                itemCount: selectedMeal.ingredients.length,
+              ),
+            ),
+          ],
+        ));
   }
 }
